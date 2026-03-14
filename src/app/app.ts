fd7acdb1c7897a6cc25fea4b8d2e2e36d1c0c135
Muhammad Ali <muhammadalimandela01@gmail.com>
2026-03-15 04:08:14 +0530

    feat: Add Vercel Analytics dependency and initialize it in the root application component.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { inject } from '@vercel/analytics';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,10 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('corely-frontend');
+
+  ngOnInit() {
+    inject();
+  }
 }
